eecc2f596808dba89d6dee4c1bf8448b20d5e381
Nishant Raj <115339821+NishantRaj278@users.noreply.github.com>
2022-10-09 12:17:53 +0530

    Add files via upload

---
+++ String2.py
@@ -0,0 +1,26 @@
+#Modify String 
+mystr="nishant raj"
+print(mystr.upper())
+
+Mystr="NISHANT RAJ"
+print(Mystr.lower())
+
+my_str="   Nishant Raj    "
+print(my_str.strip())
+
+My_str="Nishant Raj"
+print(My_str.replace("N","A"))
+
+My_Str="Nishant Raj"
+print(My_Str.split())
+
+#Concatenate String
+Name1="Nishant"
+Name2="Raj"
+Name=Name1+Name2
+print(Name)
+
+N1="Nishant"
+N2="Raj"
+N=N1+" "+N2
+print(N)
