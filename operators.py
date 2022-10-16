ab3d72dc4c28cdf5463506a5bf4368c3adf15add
Nishant Raj <115339821+NishantRaj278@users.noreply.github.com>
2022-10-16 11:41:18 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,12 @@
+a = [1,2,3,4,5,6,7,8,9,10]
+b = [1,2,3,4,5,6,7,8,9,10]
+c = a
+print(a is not c)
+
+print(b is not c)
+
+print(a != b)
+
+a = [11,22,33,44,55,66,77,88,99]
+print(88 in a)
+print(55 not in a)
